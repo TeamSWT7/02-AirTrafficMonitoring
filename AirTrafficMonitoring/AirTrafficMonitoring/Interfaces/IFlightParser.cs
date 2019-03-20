fa16e98cc00ff0ae6817dd45314276da0f6e2fd6
Christian H. Bohlbro <christian_houman@yahoo.dk>
2019-03-20 17:18:09 +0100

    Added proper flightparser interface extension

--- AirTrafficMonitoring/AirTrafficMonitoring/Interfaces/IFlightParser.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring/Interfaces/IFlightParser.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace AirTrafficMonitoring.Interfaces
 {
-    public interface IFlightParser : ISubject<IFlightParser>, IObserver<IFlightTransponderHandler>
+    public interface IFlightParser : ISubject<IFlightParser>, Interfaces.IObserver<IFlightTransponderHandler>
     {
-         
+        Flight GetNext();
+        void ParseString(string s);
+        DateTime? ParseDate(string s);
     }
 }
