d1262245a906c31020a10105f7032981f634a9d4
Mathias7120 <mgade7120@gmail.com>
2019-03-20 15:31:35 +0100

    Added Conflict and ConflictHandler

--- AirTrafficMonitoring/AirTrafficMonitoring/ConflictHandler.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring/ConflictHandler.cs
@@ -1,7 +1,58 @@
+using System;
+using System.Collections.Generic;
+
 namespace AirTrafficMonitoring
 {
-    public class ConflictHandler
+    public class ConflictHandler : IObserver<FlightValidator>
     {
+        private List<Conflict> _conflicts = new List<Conflict>();
+
         
+
+        public List<Conflict> GetConflicts()
+        {
+            return _conflicts;
+        }
+
+        public void Update(FlightValidator s)
+        {
+            
+        }
+
+        private void CheckForConflicts(Flight flight1, Flight flight2)
+        {
+            if(CheckHorisontalDistance(flight1, flight2) && CheckVerticalDistance(flight1, flight2))
+            {
+
+            }
+        }
+
+        private bool CheckHorisontalDistance(Flight flight1, Flight flight2)
+        {
+            var TempXPow = Math.Pow((flight1.position.x - flight2.position.x),2);
+            var TempYPow = Math.Pow((flight1.position.y - flight2.position.y), 2);
+            double distance = Math.Sqrt(TempXPow + TempYPow);
+
+            if (distance < 500)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool CheckVerticalDistance(Flight flight1, Flight flight2)
+        {
+            if ((flight1.altitude - flight2.altitude) < 300)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
