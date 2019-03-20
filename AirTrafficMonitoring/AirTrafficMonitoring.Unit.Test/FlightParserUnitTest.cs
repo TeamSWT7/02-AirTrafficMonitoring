5ef2f0c6354e8b7da41d9238fa69c82e01a78db7
Christian H. Bohlbro <christian_houman@yahoo.dk>
2019-03-20 16:25:18 +0100

    Added first testcase for FlightParser

--- AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/FlightParserUnitTest.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/FlightParserUnitTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using NSubstitute;
 using NUnit.Framework;
@@ -8,18 +9,29 @@ namespace AirTrafficMonitoring.Unit.Test
     [TestFixture]
     public class FlightParserUnitTest
     {
+        private ITransponderReceiver _receiver;
+        private FlightTransponderHandler _fakeFlightTransponderHandler;
         private FlightParser _uut;
 
         [SetUp]
         public void Setup()
         {
             _uut = Substitute.For<FlightParser>();
+
+            _receiver = Substitute.For<ITransponderReceiver>();
+
+            _fakeFlightTransponderHandler = Substitute.For<FlightTransponderHandler>(_receiver);
+            // _fakeFlightTransponderHandler.GetNext().Returns("ATR423;39045;12932;14000;20151006213456789");
+
+            _fakeFlightTransponderHandler.Attach(_uut);
         }
 
         [Test]
-        public void Test_()
+        public void FlightParser_OnSubjectNotify_UpdateWasCalled()
         {
-            
+            _fakeFlightTransponderHandler.Notify(_fakeFlightTransponderHandler);
+
+            _uut.Received().Update(_fakeFlightTransponderHandler);
         }
     }
 }
