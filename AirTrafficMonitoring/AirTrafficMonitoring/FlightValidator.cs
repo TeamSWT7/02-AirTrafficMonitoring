436d9c10ddc5eda687a06c10311f8e92d478da16
Axel Aaby Ulriksen <axelpanorama@hotmail.com>
2019-03-20 14:40:36 +0100

    Startet on flightValidator

--- AirTrafficMonitoring/AirTrafficMonitoring/FlightValidator.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring/FlightValidator.cs
@@ -1,7 +1,38 @@
+using System.Collections.Generic;
+
+
 namespace AirTrafficMonitoring
 {
-    public class FlightValidator
+    public class FlightValidator : Subject<FlightValidator>, IObserver<FlightHandler>
+
     {
-        
+        private List<Flight> _flights = new List<Flight>();
+
+        public void Update(FlightHandler fh)
+        {
+            string next = fh.GetFlight();
+            while (next != null)
+            {
+                ParseString(next);
+
+                next = fh.GetFlight();
+            }
+
+            Notify(this);
+        }
+
+        public Flight GetNext()
+        {
+            if (_flight.Count > 0)
+            {
+                string r = _flight[0];
+
+                _flightStrings.RemoveAt(0);
+
+                return r;
+            }
+
+            return null;
+        }
     }
 }
