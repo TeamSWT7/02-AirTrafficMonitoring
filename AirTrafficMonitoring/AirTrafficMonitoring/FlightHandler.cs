c9ba99c40e7fbbfac5fa29b07d662cc4dc532c96
Nikolaj R. Molzen <nikolaj_richardy@hotmail.com>
2019-03-20 14:40:34 +0100

    Added a rough update function and the GetFlights function

--- AirTrafficMonitoring/AirTrafficMonitoring/FlightHandler.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring/FlightHandler.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
+
 namespace AirTrafficMonitoring
 {
-    public class FlightHandler
+    public class FlightHandler : Subject<FlightHandler>, IObserver<FlightParser>
     {
-        
+        private List<Flight> _flights = new List<Flight>();
+        private List<Flight> _newFlights = new List<Flight>();
+
+        public void Update(FlightParser flightParser)
+        {
+            Flight next = flightParser.GetNext();
+            while (next != null)
+            {
+                _flights.Add(flightParser.GetNext());
+            }
+            Notify(this);
+        }
+
+        public List<Flight> GetFlights()
+        {
+            return _flights;
+        }
     }
 }
