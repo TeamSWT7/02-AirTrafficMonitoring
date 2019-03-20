f731ef3760f57f6798897bff63eb2235da228367
Nikolaj R. Molzen <nikolaj_richardy@hotmail.com>
2019-03-20 21:05:21 +0100

    Added interfaces and made a first draft for UpdateList

--- AirTrafficMonitoring/AirTrafficMonitoring/FlightHandler.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring/FlightHandler.cs
@@ -1,35 +1,40 @@
+using System;
 using System.Collections.Generic;
+using AirTrafficMonitoring.Interfaces;
 
 namespace AirTrafficMonitoring
 {
-    public class FlightHandler : Subject<FlightHandler>, IObserver<FlightParser>
+    public class FlightHandler : Subject<IFlightHandler>, IFlightHandler
     {
         private List<Flight> _flights = new List<Flight>();
-        private List<Flight> _newFlights = new List<Flight>();
+        private List<Flight> _tempFlights = new List<Flight>();
 
         public void Update(FlightParser flightParser)
         {
             Flight next = flightParser.GetNext();
             while (next != null)
             {
-                _newFlights.Add(flightParser.GetNext());
-                UpdateList(_newFlights, _flights);
+                _tempFlights = _flights;
+                UpdateList(next);
                 next = flightParser.GetNext();
             }
             Notify(this);
         }
 
-        private void UpdateList(List<Flight> newList, List<Flight> list)
+        private void UpdateList(Flight next)
         {
-            if (list.Capacity == 0)
+            int f = 0;
+            for (int i = 0; i < _flights.Capacity; i++)
             {
-                list = newList;
-            }
-            else
-            {
-                foreach (var fligth in newList)
+                if (next.tag == _tempFlights[i].tag)
                 {
-                    
+                    UpdateFlightInfo(next, _tempFlights[i], _flights[i]);
+                    _flights[i] = _tempFlights[i];
+                    f = 1;
+                }
+                else if(i == _flights.Capacity -1 && f != 1)
+                {
+                    _flights.Add(next);
                 }
             }
         }
---
+++ AirTrafficMonitoring/AirTrafficMonitoring/Interfaces/IFlightHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirTrafficMonitoring.Interfaces
+{
+    public interface IFlightHandler : ISubject<IFlightHandler>, Interfaces.IObserver<FlightParser>
+    {
+        List<Flight> GetFlights();
+    }
+}
