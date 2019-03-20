7d063c365b35111339e47e0ada3a0c44f6762765
Axel Aaby Ulriksen <axelpanorama@hotmail.com>
2019-03-20 21:17:37 +0100

    Trying to make test cases

---
+++ AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/FlightValidaterUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NSubstitute.ReceivedExtensions;
+using NUnit.Framework;
+using TransponderReceiver;
+
+namespace AirTrafficMonitoring.Unit.Test
+{
+    [TestFixture]
+    public class FlightValidaterUnitTest
+    {
+
+        private ITransponderReceiver _receiver;
+        private FlightTransponderHandler _uut;
+        private RawTransponderDataEventArgs _testEventArgs;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = Substitute.For<FlightValidator>();
+            _receiver = Substitute.For<ITransponderReceiver>();
+            _fakeFlightHandler = Substitute.For<FlightHandler>(_reciver);
+        }
+
+
+        [test]
+        public void
+        {
+            _fakeFlightHandler
+    }
+
+    }
+}
