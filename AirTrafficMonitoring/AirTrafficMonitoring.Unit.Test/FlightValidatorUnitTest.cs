2bece92115e293ccda8c393d8a048b9990984baf
Axel Aaby Ulriksen <axelpanorama@hotmail.com>
2019-03-21 17:31:43 +0100

    Updated IFlightValidator with correct function + Startet Unitest for FlightValidator

---
+++ AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/FlightValidatorUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitoring.Interfaces;
+using NSubstitute;
+using NSubstitute.ReceivedExtensions;
+using NUnit.Framework;
+using TransponderReceiver;
+
+namespace AirTrafficMonitoring.Unit.Test
+{
+    [TestFixture]
+    public class FlightValidatorUnitTest
+    {
+        private FlightValidator _uut;
+        private Flight _flight;
+        private Airspace _airspace;
+
+        [SetUp]
+        public void Setup()
+        {
+            
+            //crating fakes
+            _uut = Substitute.For<FlightValidator>();
+
+
+        }
+    }
+}
