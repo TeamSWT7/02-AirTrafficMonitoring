c9ee5b2295133d0148d7a43340bbec41d48911a2
Nikolaj R. Molzen <nikolaj_richardy@hotmail.com>
2019-03-21 11:31:49 +0100

    Added UnitTest file

---
+++ AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/FlightHandlerUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using NSubstitute;
+using NSubstitute.ReceivedExtensions;
+using NUnit.Framework;
+using TransponderReceiver;
+
+namespace AirTrafficMonitoring.Unit.Test
+{
+    [TestFixture]
+    public class FlightHandlerUnitTest
+    {
+        
+    }
+}
