f9a8e0d7f8672c718493d5aa243c277e5924973c
Nikolaj R. Molzen <nikolaj_richardy@hotmail.com>
2019-03-21 18:23:59 +0100

    Added two passed test cases: One for calculate direction and one for calculate velocity

--- AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/FlightHandlerUnitTest.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/FlightHandlerUnitTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using AirTrafficMonitoring.Interfaces;
 using NSubstitute;
 using NSubstitute.ReceivedExtensions;
@@ -39,6 +40,40 @@ namespace AirTrafficMonitoring.Unit.Test
             };
             Assert.AreEqual(279.46,_uut.CalculateDirection(_flight1,_flight2),0.1);
         }
+        [Test]
+        public void CalculateDirection_NewUpdateSameYCoordinate_ResultIsCorrect()
+        {
+            _flight1 = new Flight()
+            {
+                position = new Coords(1000, 2000, 3000)
+            };
+            _flight2 = new Flight()
+            {
+                position = new Coords(1500, 2000, 3000)
+            };
+            Assert.AreEqual(0, _uut.CalculateDirection(_flight1,_flight2));
+        }
+
+        #endregion
+
+        #region CalculateVelocity
+
+        [Test]
+        public void CalculateVelocity_NewUpdateSimpleChange_ResultIsCorrect()
+        {
+            _flight1 = new Flight()
+            {
+                position = new Coords(1000, 2000, 3000),
+                timestamp = new DateTime(2019, 03, 21, 18, 10, 0)
+            };
+            _flight2 = new Flight()
+            {
+                position = new Coords(2000, 2000, 3000),
+                timestamp = new DateTime(2019, 03, 21, 18, 10, 5)
+            };
+            Assert.AreEqual(200, _uut.CalculateVelocity(_flight1,_flight2));
+        }
+
         #endregion
     }
 }
