3c5024a187d77d8e82b846db2a07161857f8889f
Nikolaj R. Molzen <nikolaj_richardy@hotmail.com>
2019-03-21 11:45:01 +0100

    Added setup function, ready to create tests

--- AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/FlightHandlerUnitTest.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/FlightHandlerUnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using AirTrafficMonitoring.Interfaces;
 using NSubstitute;
 using NSubstitute.ReceivedExtensions;
 using NUnit.Framework;
@@ -9,6 +10,17 @@ namespace AirTrafficMonitoring.Unit.Test
     [TestFixture]
     public class FlightHandlerUnitTest
     {
-        
+        private FlightHandler _uut;
+        private IFlightTransponderHandler _fakeFlightTransponderHandler;
+        private Flight _flight1;
+        private Flight _flight2;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Create attributes for tests
+            _fakeFlightTransponderHandler = Substitute.For<IFlightTransponderHandler>();
+            _uut = Substitute.For<FlightHandler>();
+        }
     }
 }
