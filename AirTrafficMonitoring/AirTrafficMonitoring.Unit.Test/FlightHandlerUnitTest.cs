a194ae746b594f45ef25953f67a480e7b7149e08
Christian H. Bohlbro <christian_houman@yahoo.dk>
2019-03-21 21:40:22 +0100

    Add FlightHandler test cases for Update method

--- AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/FlightHandlerUnitTest.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/FlightHandlerUnitTest.cs
@@ -14,44 +14,92 @@ namespace AirTrafficMonitoring.Unit.Test
         #region Setup
         private FlightHandler _uut;
         private IFlightParser _fakeFlightParser;
-        private Flight _flight1;
-        private Flight _flight2;
+        private List<Flight> _multipleFlights;
 
         [SetUp]
         public void Setup()
         {
             //Create attributes for tests
             _fakeFlightParser = Substitute.For<IFlightParser>();
+
             _uut = Substitute.For<FlightHandler>();
+
+            _multipleFlights = new List<Flight>
+            {
+                new Flight()
+                {
+                    tag = "ATR423",
+                    position = new Coords(39045, 12932, 14000),
+                },
+                new Flight()
+                {
+                    tag = "BCD123",
+                    position = new Coords(10005, 85890, 12000),
+                },
+                new Flight()
+                {
+                    tag = "XYZ987",
+                    position = new Coords(25059, 75654, 4000),
+                }
+            };
         }
         #endregion
 
+        [Test]
+        public void Update_MultipleValues_ListIsCorrect()
+        {
+            _fakeFlightParser.GetNext().Returns(
+                x => _multipleFlights[0],
+                x => _multipleFlights[1],
+                x => _multipleFlights[2],
+                x => null
+            );
+
+            _uut.Update(_fakeFlightParser);
+
+            Assert.AreEqual(_uut.GetFlights(), _multipleFlights);
+        }
+
+        [Test]
+        public void Update_EmptyValues_ListIsEmpty()
+        {
+            _fakeFlightParser.GetNext().Returns(
+                x => null
+            );
+
+            _uut.Update(_fakeFlightParser);
+
+            Assert.AreEqual(_uut.GetFlights().Count, 0);
+        }
+
         #region CalculateDirection
         [Test]
-        public void CalculateDirection_NewUpdate_ResultIsCorrect()
+        public void CalculateDirection_TwoValues_DirectionIsCorret()
         {
-            _flight1 = new Flight()
+            Flight flight1 = new Flight()
             {
                 position = new Coords(1000, 2000, 3000)
             };
-            _flight2 = new Flight()
+            Flight flight2 = new Flight()
             {
                 position = new Coords(1200, 800, 3000)
             };
-            Assert.AreEqual(279.46,_uut.CalculateDirection(_flight1,_flight2),0.01);
+
+            Assert.AreEqual(279.46, _uut.CalculateDirection(flight1, flight2), 0.01);
         }
         [Test]
-        public void CalculateDirection_NewUpdateSameYCoordinate_ResultIsCorrect()
+        public void CalculateDirection_FlightsSameYCoordinate_DirectionIsCorrect()
         {
-            _flight1 = new Flight()
+            Flight flight1 = new Flight()
             {
                 position = new Coords(1000, 2000, 3000)
             };
-            _flight2 = new Flight()
+            Flight flight2 = new Flight()
             {
                 position = new Coords(1500, 2000, 3000)
             };
-            Assert.AreEqual(0, _uut.CalculateDirection(_flight1,_flight2));
+
+            Assert.AreEqual(0, _uut.CalculateDirection(flight1, flight2));
         }
 
         #endregion
@@ -61,33 +109,35 @@ namespace AirTrafficMonitoring.Unit.Test
         [Test]
         public void CalculateVelocity_NewUpdateSimpleChange_ResultIsCorrect()
         {
-            _flight1 = new Flight()
+            Flight flight1 = new Flight()
             {
                 position = new Coords(1000, 2000, 3000),
                 timestamp = new DateTime(2019, 03, 21, 18, 10, 0)
             };
-            _flight2 = new Flight()
+            Flight flight2 = new Flight()
             {
                 position = new Coords(2000, 2000, 3000),
                 timestamp = new DateTime(2019, 03, 21, 18, 10, 5)
             };
-            Assert.AreEqual(200, _uut.CalculateVelocity(_flight1,_flight2));
+
+            Assert.AreEqual(200, _uut.CalculateVelocity(flight1, flight2));
         }
 
         [Test]
         public void CalculateVelocity_NewUpdateCompleteChange_ResultIsCorrect()
         {
-            _flight1 = new Flight()
+            Flight flight1 = new Flight()
             {
                 position = new Coords(1000, 2000, 3000),
                 timestamp = new DateTime(2019, 03, 21, 18, 10, 0)
             };
-            _flight2 = new Flight()
+            Flight flight2 = new Flight()
             {
                 position = new Coords(800, 2400, 4500),
                 timestamp = new DateTime(2019, 03, 21, 18, 10, 10)
             };
-            Assert.AreEqual(156.52, _uut.CalculateVelocity(_flight1, _flight2),0.01);
+
+            Assert.AreEqual(156.52, _uut.CalculateVelocity( flight1, flight2), 0.01);
         }
 
         #endregion
